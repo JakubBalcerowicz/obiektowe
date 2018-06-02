b128f2a18d39e299399be42c4dec03a9d86b4609
Jakub <ku44@wp.pl>
2018-06-02 20:54:39 +0200

    Update dasd.cs

--- dasd.cs
+++ dasd.cs
@@ -1,4 +1,20 @@
-public void LoadByIndex(int sceneIndex)
+public class AroundYaxis : MonoBehaviour
+{
+
+    float rotationNumber = 10;
+    private float nextActionTime = 0.0f;
+    private float period = 0.1f;
+    void Update()
     {
-        SceneManager.LoadScene(sceneIndex);
+        //timeSinceLevelLoad time for scene started to move obcjekt axe Z
+        if (Time.timeSinceLevelLoad >= nextActionTime)
+        {
+            nextActionTime += period;
+            var rotationVector = transform.rotation.eulerAngles;
+            rotationVector.y = rotationVector.y + rotationNumber;
+            transform.rotation = Quaternion.Euler(rotationVector);
+        }
+
+
     }
+}
